59d39e080d02b95bf94a9c1e9f300e1e711bc57d
Даниил Тертень <daniil.terten@monopoly.su>
2022-07-22 15:17:17 +0300

    delete comments

--- StockApp/SortPalletsExtension.cs
+++ StockApp/SortPalletsExtension.cs
@@ -7,15 +7,6 @@ namespace StockApp
 {
     public static class SortPalletsExtension
     {
-        //static void Main(string[] args)
-        //{
-        //    var s = Warehouse.ReadFromFile();
-        //    s.FirstOrDefault().Height = 100000;
-        //    var sortedPallets = s.OrderByDescending(x => x.ExpirationDate).Take(3);
-        //    Warehouse.PrintPalletOnConsole(sortedPallets);
-        //    Warehouse.WriteInFile(s);
-        //}
-
         public static List<IGrouping<DateTime?, PalletModel>> GroupPalletsByExpirationDate(this List<PalletModel> palletModels)
         {
             return palletModels.GroupBy(x => x.ExpirationDate).ToList();
